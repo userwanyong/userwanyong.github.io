c150f84ebcca9ba70f878feabc60a564b3943ecd
userwanyong <2026804718@qq.com>
2025-02-25 23:45:40 +0800

    feat: init project

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,7 +3,6 @@ import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import './hidden.css'
 import MyLayout from './components/MyLayout.vue'
-import 'nprogress-v2/dist/index.css' // 进度条样式
 
 export default {
     extends: DefaultTheme,
